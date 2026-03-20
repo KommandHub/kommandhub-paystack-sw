c5944459ef753859e4b6172748c30493618d2d71
Anitche Chisom <admin@kommandhub.com>
2026-03-20 21:14:45 +0100

    Update

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// If running in CI → skip Shopware bootstrap
+if (getenv('CI') === 'true') {
+    require __DIR__ . '/../vendor/autoload.php';
+    return;
+}
+
+// Otherwise (local dev) → use Shopware bootstrap
+require __DIR__ . '/TestBootstrap.php';
